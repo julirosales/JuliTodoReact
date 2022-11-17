48c79d5a951720dc8887cb0a3cee28c23a220798
SIAF AC Rosales Julieta Agostina <jarosales@ara.mil.ar>
2022-11-17 13:25:01 -0300

    agrego css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,9 +5,11 @@ import { CreateButton } from "../CreateButton";
 import { Items } from "../Item";
 import { FormItem } from "../FormItem";
 import { ListItems } from "../ListItems";
+import "../Loading/index.css"
 
 function App() {
   let arrayTodos = [];
+  const [loading,setLoading] = useState(true)
   const [arrayTodo, setArrayTodo] = useState(arrayTodos);
 
   useEffect(() => {
@@ -16,10 +18,12 @@ function App() {
       fetch("https://6348a6070b382d796c74f065.mockapi.io/api/v1/todos")
         .then((response) => response.json())
         .then((data) => {
+          setLoading(false);
           setArrayTodo(data);
         })
         .catch((error) => console.log(error));
-    }, 1200);
+        
+    }, 1300);
   }, []);
   return (
     <React.Fragment>
@@ -27,6 +31,7 @@ function App() {
       <Search />
       <CreateButton />
       <ListItems>
+        {loading && <div className="cargando"></div>}
         {arrayTodo.map((todo) => (
           <Items
             titulo={todo.titulo}
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BsCheck2All } from "react-icons/bs";
 import { AiOutlineEdit } from "react-icons/ai";
 import { CiTrash } from "react-icons/ci";
-import "./Item.css";
+import "../Item/index.css";
 
 function Items(props) {
   return (
     <React.Fragment>
-      <li className="li">
-        <BsCheck2All />
+      <div className="container-tarea">
+        <BsCheck2All className="listNoFinalizada"/>
         <div className="divTarea">
           <h4 className="tituloLista" maxLength="40">
             {props.titulo}
@@ -16,10 +16,10 @@ function Items(props) {
           <p className="descripcionLista"> {props.descripcion}</p>
         </div>
         <div>
-          <AiOutlineEdit />
-          <CiTrash />
+          <AiOutlineEdit className="btnEdit"/>
+          <CiTrash className="btnBorrar" />
         </div>
-      </li>
+      </div>
     </React.Fragment>
   );
 }
